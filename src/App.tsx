bd36c1a54139199194f3957e4f754a394a976808
KosarMyatyi <nespovityi_oleg@mail.ru>
2023-03-14 17:31:57 +0300

    01;

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { TasksType, Todolist } from './Todolist';
 
 function App() {
+
+  let tasks1: Array<TasksType> = [
+    { id: 1, title: 'HTML&CSS', isDone: true },
+    { id: 2, title: 'JS', isDone: true },
+    { id: 3, title: 'React&Redux', isDone: true }
+  ]
+
+  let tasks2: Array<TasksType> = [
+    { id: 1, title: 'Terminator', isDone: true },
+    { id: 2, title: 'Avatar', isDone: true },
+    { id: 3, title: 'The Good the Bad and the Ugly', isDone: true }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todolist title='What to learn' tasks={tasks1} />
+      <Todolist title='Movies' tasks={tasks2} />
     </div>
   );
 }
+
 
 export default App;
